819c9a51fdab2a6a8c4e0d300cc3b19fe7393227
Geula <gkatan@gmail.com>
2020-06-21 15:01:39 +0300

    updated photoes

--- src/app/gallery/gallery-image/gallery-image.component.ts
+++ src/app/gallery/gallery-image/gallery-image.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit, Input } from '@angular/core';
 export class GalleryImageComponent implements OnInit {
   @Input('imgSource') source: string;
   @Input('imgName') name: string;
-  
   constructor() { }
 
   ngOnInit() {
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -12,17 +12,16 @@ images = [
   new GalleryImage ('Eye of the Dragon', 'assets/Paintings/Eye of the Dragon.jpg'),
   new GalleryImage ('Dancing Lavendar', 'assets/Paintings/Dancing Lavendar.jpg'),
   new GalleryImage ('Together in the Rain', 'assets/Paintings/Together in the Rain.jpg'),
-  new GalleryImage ('Wave', 'assets/Paintings/Wave.jpg'),
-  new GalleryImage ('Campfire on the Beach', 'assets/Paintings/Campfire on the Beach.jpg'),
+  new GalleryImage ('Cat', 'assets/Paintings/Cat.jpg'),
+  new GalleryImage ('Forest', 'assets/Paintings/Forest.jpg'),
   new GalleryImage ('Orchids in Bloom', 'assets/Paintings/Orchids in Bloom.jpg'),
   new GalleryImage ('Stargazing Adventure', 'assets/Paintings/Stargazing Adventure.jpg'),
   new GalleryImage ('Canon Beach', 'assets/Paintings/Canon Beach.jpg'),
-  new GalleryImage ('Summer Stream', 'assets/Paintings/Summer Stream.jpg'),
-  new GalleryImage ('Aurora Borealis', 'assets/Paintings/Aurora Borealis.jpg'),
-  new GalleryImage ('Betta Fish', 'assets/Paintings/Betta Fish.jpg'),
-  new GalleryImage ('Blue Rose', 'assets/Paintings/Blue Rose.jpg'),
-  new GalleryImage ('Rose', 'assets/Paintings/Rose.jpg'),
-  new GalleryImage ('Apple Blossoms', 'assets/Paintings/Apple Blossoms.jpg')
+  new GalleryImage ('Horse at Sunrise', 'assets/Paintings/Horse at Sunrise.jpg'),
+  new GalleryImage ('Mount Hood', 'assets/Paintings/Mount Hood.jpg'),
+  new GalleryImage ('Toothless and Hiccup', 'assets/Paintings/Toothless and Hiccup.jpg'),
+  new GalleryImage ('Tree at Moonrise', 'assets/Paintings/Tree at Moonrise.jpg'),
+  new GalleryImage ('Rose', 'assets/Paintings/Rose.jpg')
 ];
 
   constructor() { }
